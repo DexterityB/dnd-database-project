dc7c17db21544c2e2cafa8268af933a79252ea3b
Blanchard <blanchardd@CSGP.EDU>
2026-04-29 08:28:35 -0600

    Slight updates

--- main.py
+++ main.py
@@ -18,6 +18,38 @@ def create_connection():
         print(f'❌ Error: "{e}" ❌')
         return False
 
+def view_table(connection, table):
+    try:
+        cursor = connection.cursor()
+        query = f"SELECT * FROM {table}"
+        cursor.execute(query)
+        table = cursor.fetchall()
+
+        sec_len = 0
+        for num in range(len(table)):
+            first_len = sec_len
+            try:
+                sec_len = len(str(table[num]))
+            except:
+                sec_len = 0
+
+            if first_len > sec_len:
+                print("-" * first_len)
+            else:
+                print("-" * sec_len)
+
+            print("|", end = " ")
+            for value in table[num]:
+                print(value, end = " | ")
+            print("")
+        print("-" * sec_len)
+
+    except Error as e:
+        print(f'❌ Error: "{e}" ❌')
+
+    finally:
+        return None
+
 def add_character(connection, name, dnd_class, description, level):
     '''Add a character to the database'''
     try:
@@ -27,25 +59,31 @@ def add_character(connection, name, dnd_class, description, level):
         connection.commit()
         print(f'✅ "{name}" added to database ✅')
 
+        cursor.execute("SELECT id, name FROM characters WHERE name = %s", (name,))
+        id = int(cursor.fetchall()[0])
+
+    except Error as e:
+        print(f'❌ Error: "{e}" ❌')
+        id = None
+        
+    finally:
+        return id
+
+def add_character(id, strength, dexterity, constitution, intelligence, wisdom, charisma):
+    '''Add a character's stats to the database'''
+    try:
+        cursor = connection.cursor()
+        query = "INSERT INTO stats (id, strength, dexterity, constitution, intelligence, wisdom, charisma) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        cursor.execute(query, (id, strength, dexterity, constitution, intelligence, wisdom, charisma))
+        connection.commit()
+        print(f'✅ Stats added to database ✅')
+
+        #cursor.execute("SELECT id, name FROM characters WHERE name = %s", (name,))
+        #id = int(cursor.fetchall()[0])
+
     except Error as e:
         print(f'❌ Error: "{e}" ❌')
         
-    finally:
-        return None
-
-def view_table(connection, table):
-    try:
-        cursor = connection.cursor()
-        query = f"SELECT * FROM {table}"
-        cursor.execute(query)
-        table = cursor.fetchall()
-        for row in table:
-            print(row)
-        #connection.commit()
-
-    except Error as e:
-        print(f'❌ Error: "{e}" ❌')
-
     finally:
         return None
 
@@ -58,24 +96,38 @@ def main():
         return None
     
     while True:
-        print("\n1. Add Character")
-        print("2. View All Characters")
+        print("\n1. View Table")
+        print("2. Add Character")
         print("3. Exit")
         
         select = input("Selection: ")
         
-        if select == "1":
-            name = input("Name: ")
-            dnd_class = input("Class: ")
-            level = input("Level: ")
-            description = input("Description: ")
-            add_character(connection, name, dnd_class, description, level)
+        match select:
+            case "1":
+                table = input("Table name: ").lower()
+                print("")
+                view_table(connection, table)
 
-        elif select == "2":
-            view_table(connection, 'characters')
+            case "2":
+                name = input("Name: ")
+                dnd_class = input("Class: ")
+                level = input("Level: ")
+                description = input("Description: ")
+                id = add_character(connection, name, dnd_class, description, level)
 
-        elif select == "3":
-            break
+                auto - input("\nWould you like to input your statistics or randomize them automatically [manual/auto]: ").lower()
+                if auto == "manual":
+                    strength = int(input("Strength: "))
+                    dexterity = int(input("Dexterity: "))
+                    constitution = int(input("Constitution: "))
+                    intelligence = int(input("Intelligence: "))
+                    wisdom = int(input("Wisdom: "))
+                    charisma = int(input("Charisma: "))
+                
+                add_stats(id, strength, dexterity, constitution, intelligence, wisdom, charisma)
+
+            case "3":
+                break
     
     connection.close()
 
